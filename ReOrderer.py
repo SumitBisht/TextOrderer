86893af86f396efafa4556ccebed1accaec8c706
sumitbisht4u <sumitbisht1987@gmail.com>
2012-07-17 15:36:58 +0530

    added the manual ordering specifier

--- ReOrderer.py
+++ ReOrderer.py
@@ -2,7 +2,7 @@ import wx
 import os
 import random
 '''
-This is the main class that provides the UI and logic for the text block randomization.
+This is the main class that provides both the UI and logic for the text randomization.
 '''
 class ReOrderer(wx.Frame):
 
@@ -47,12 +47,16 @@ class ReOrderer(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnSaveToFile, self.save)
         self.run = wx.Button(self, -1, "Randomize")
         self.Bind(wx.EVT_BUTTON, self.OnRandomize, self.run)
+        self.orderuser = wx.Button(self, -1, "Use Order")
+        self.Bind(wx.EVT_BUTTON, self.OnUseOrder, self.orderuser)
         self.save.Disable()
         self.run.Disable()
+        self.orderuser.Disable()
         
         buttonsizer = wx.BoxSizer(wx.HORIZONTAL)
-        buttonsizer.Add(self.save, 1, wx.EXPAND)
+        buttonsizer.Add(self.save, 3, wx.EXPAND)
         buttonsizer.Add(self.run, 2, wx.EXPAND)
+        buttonsizer.Add(self.orderuser, 3, wx.EXPAND)
         mainsizer.Add(buttonsizer)
         self.SetSizerAndFit(mainsizer)
         self.SetMinSize(self.GetSize())
@@ -71,10 +75,8 @@ class ReOrderer(wx.Frame):
         if len(paperContent) == 0:
             self.GiveError('Nothing to randomize')
             return
-        splits = paperContent.split(self.splitDelim)
-        print "splitted contents are:"
-        for i in Range(0, len(splits)):
-            splits[i] = splits[i].strip(self.splitDelim)
+        splits = self.splitRawData(paperContent)
+        
         random.shuffle(splits)
         newContent = ""
         self.content.SetValue("")
@@ -82,6 +84,21 @@ class ReOrderer(wx.Frame):
             newContent += item
             newContent += self.splitDelim
         self.content.SetValue(newContent.rstrip(self.splitDelim))
+        self.OnQuestionOrderSelect(event)
+
+    def splitRawData(self, inputData):
+        splits = inputData.split(self.splitDelim)
+        result = []
+        for i in range(0, len(splits)):
+            validQuestion = True
+            content = splits[i]
+            options = ['(A)', '(B)', '(C)', '(D)']
+            for var in options:
+                if not content.__contains__(var):
+                   validQuestion = False
+            if validQuestion == True:
+                result.append(content)
+        return result
 
     def OnFileSelect(self, event):
         fileChoices = "Text file |*.txt|All files|*.*"
@@ -91,28 +108,60 @@ class ReOrderer(wx.Frame):
             selectedFileName = extractedFile.GetPath()
             self.selectedFileText.SetValue(selectedFileName)
             self.populateFields(selectedFileName)
+            self.OnQuestionOrderSelect(event)
 
             self.save.Enable()
             self.run.Enable()
+            self.orderuser.Enable()
         extractedFile.Destroy()
 
     def populateFields(self, filename):
+        self.content.SetValue('')
         testFileName = open(filename)
         contents = testFileName.readlines()
         for line in contents:
             self.content.WriteText(str(line))
 
+    #Selects the question number and populates it in a csv format on the textbox to assist
+    #in the manual entry.
     def OnQuestionOrderSelect(self, event):
         orderingSerial = ''
-        content = self.content.GetValue()
-        if len(content) == 0:
+        contents = self.content.GetValue()
+        if len(contents) == 0:
             self.GiveError('Unable to load questions for counting')
             return
-        questionCount = len(content.split(self.splitDelim))
+        
+        questions = contents.split(self.splitDelim)
+        questionCount = len(questions)
+        
         for cnt in range(0, questionCount):
-            orderingSerial+=str(cnt)+', '
-        self.orderField.SetValue("")
+            question = questions[cnt]
+            orderingSerial+=str(question[:question.index('.')])+', '
+
+        self.orderField.SetValue('')
         self.orderField.SetValue(orderingSerial[:-2])
+
+    #set the order of tests on the basis of specified test order
+    def OnUseOrder(self, event):
+        order = self.orderField.GetValue()
+        if order == '':
+            return
+        questions = self.splitRawData(self.content.GetValue())
+        order = order.split(',')
+        details = dict()
+
+        #Sort the questions according to the specified numbers, using a dictionary
+        for ques in questions:
+            details[ques[:ques.index('.')].strip()] = ques
+
+        self.content.SetValue('')
+        self.orderField.SetValue('')
+        newcontent = ''
+        for question in order:
+            newcontent += str(details.pop(question.strip()) + self.splitDelim)
+        newcontent = newcontent[:-len(self.splitDelim)]
+        self.content.SetValue(newcontent)
+        self.OnQuestionOrderSelect(event)
 
     # Provides error dialogs in a resuable manner.
     # Is based on the presence of loaded file contents
